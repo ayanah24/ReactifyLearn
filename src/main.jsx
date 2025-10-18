82c333f795f5008b8ef5487144a4fad27aaca01e
ayanah24 <ayan27452@gmail.com>
2025-10-18 23:44:51 +0530

    added Github component and link in Header and also updated main.jsx to include the new route.

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 //  const router = createBrowserRouter([
 //   {
@@ -38,7 +39,12 @@ const router= createBrowserRouter(
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
       <Route path='user/:userid' element={<User />} />
-     </Route>
+      <Route 
+      loader={githubInfoLoader}
+      path='Github'
+      element={<Github/>}
+      />
+      </Route>
   )
 )
 
